d21e7d7f2302761e90232ef3eb6a216dbfe1de92
LIN DING <lin.ding@bytedance.com>
2026-03-03 15:54:26 +0800

    fix: fix type

--- packages/opencode/src/flag/flag.ts
+++ packages/opencode/src/flag/flag.ts
@@ -9,6 +9,7 @@ export namespace Flag {
   export const OPENCODE_CONFIG = process.env["OPENCODE_CONFIG"]
   export declare const OPENCODE_TUI_CONFIG: string | undefined
   export declare const OPENCODE_CONFIG_DIR: string | undefined
+  export declare const OPENCODE_MCP_CONFIG_DIR: string | undefined
   export const OPENCODE_CONFIG_CONTENT = process.env["OPENCODE_CONFIG_CONTENT"]
   export const OPENCODE_DISABLE_AUTOUPDATE = truthy("OPENCODE_DISABLE_AUTOUPDATE")
   export const OPENCODE_DISABLE_PRUNE = truthy("OPENCODE_DISABLE_PRUNE")
